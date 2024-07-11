439b17373d4d1bb23af2e8da060cb8e3896f0aa5
Amirhosein Kheirandish <kheirandishamirhosein@gmail.com>
2024-07-12 00:21:49 +0330

    change MovieCard

--- app/src/main/java/com/example/movieapp/presentation/ui/popular/movieList/compose/MovieCard.kt
+++ app/src/main/java/com/example/movieapp/presentation/ui/popular/movieList/compose/MovieCard.kt
@@ -1,9 +1,13 @@
 package com.example.movieapp.presentation.ui.popular.movieList.compose
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -18,6 +22,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
@@ -35,35 +40,67 @@ fun MovieCard(movie: ResultMovie) {
         modifier = Modifier
             .fillMaxWidth()
             .height(250.dp)
-            .padding(8.dp)
-            .clip(RoundedCornerShape(16.dp)),
+            .padding(8.dp),
         elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
     ) {
-        Column(modifier = Modifier.fillMaxWidth()) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.Black.copy(alpha = 0.9f))
+        ) {
             Image(
                 painter = rememberImagePainter("https://image.tmdb.org/t/p/w500${movie.backdropPath}"),
                 contentDescription = null,
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .height(150.dp)
-                    .clip(RoundedCornerShape(16.dp)),
+                    .fillMaxSize()
+                    .alpha(0.5f),
                 contentScale = ContentScale.Crop
             )
-            Spacer(modifier = Modifier.height(8.dp))
-            Text(
-                text = movie.title,
-                fontSize = 20.sp,
-                fontWeight = FontWeight.Bold,
-                modifier = Modifier.padding(horizontal = 8.dp)
-            )
-            Spacer(modifier = Modifier.height(4.dp))
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier.padding(horizontal = 8.dp)
+
+            Box(
+                modifier = Modifier
+                    .height(200.dp)
+                    .width(150.dp)
+                    .align(Alignment.CenterEnd)
+                    .padding(end = 20.dp, top = 8.dp, bottom = 8.dp)
+                    .clip(RoundedCornerShape(8.dp))
             ) {
-                Icon(Icons.Default.Star, contentDescription = "Vote", tint = Color.Yellow)
-                Spacer(modifier = Modifier.width(4.dp))
-                Text(text = "${movie.voteAverage}")
+                Image(
+                    painter = rememberImagePainter("https://image.tmdb.org/t/p/w500${movie.posterPath}"),
+                    contentDescription = null,
+                    modifier = Modifier.fillMaxSize(),
+                    contentScale = ContentScale.Crop
+                )
+            }
+
+            Row(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Column(
+                    modifier = Modifier.weight(1f)
+                ) {
+                    Text(
+                        text = movie.title,
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.White
+                    )
+                    Spacer(modifier = Modifier.height(4.dp))
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                    ) {
+                        Icon(Icons.Default.Star, contentDescription = "Vote", tint = Color.Yellow)
+                        Spacer(modifier = Modifier.width(4.dp))
+                        Text(
+                            text = "${movie.voteAverage}",
+                            color = Color.White
+                        )
+                    }
+                }
             }
         }
     }
