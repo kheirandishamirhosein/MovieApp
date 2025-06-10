fa0cfcc95332a77cd160b953264fdbb8fad8f3dd
Amirhosein Kheirandish <kheirandishamirhosein@gmail.com>
2025-06-11 00:16:41 +0330

    Feat: Add TopAppBar to upcoming Movies screen

--- app/src/main/java/com/example/movieapp/presentation/upcoming/UpcomingMoviesListScreen.kt
+++ app/src/main/java/com/example/movieapp/presentation/upcoming/UpcomingMoviesListScreen.kt
@@ -1,11 +1,16 @@
 package com.example.movieapp.presentation.upcoming
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -19,6 +24,7 @@ import com.example.movieapp.presentation.state.ResultStates
 import com.example.movieapp.presentation.upcoming.viewmodel.UpcomingMovieViewModel
 import com.example.movieapp.presentation.upcoming.viewmodel.UpcomingUiEvent
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun UpcomingMoviesScreen(viewModel: UpcomingMovieViewModel = hiltViewModel()) {
     val upcomingMovies by viewModel.upcomingMoviesState.collectAsState()
@@ -26,29 +32,37 @@ fun UpcomingMoviesScreen(viewModel: UpcomingMovieViewModel = hiltViewModel()) {
     LaunchedEffect(Unit) {
         viewModel.onEvent(UpcomingUiEvent.LoadUpcomingMovies)
     }
+    Scaffold(
+        topBar = {
+            TopAppBar(title = { Text(text = "Upcoming Movies") })
+        },
+        content = { paddingValues ->
+            Column(modifier = Modifier.padding(paddingValues)) {
+                when (upcomingMovies) {
+                    is ResultStates.Loading -> {
+                        Box(
+                            modifier = Modifier.fillMaxSize(),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            CircularProgressIndicator()
+                        }
+                    }
 
-    when (upcomingMovies) {
-        is ResultStates.Loading -> {
-            Box(
-                modifier = Modifier.fillMaxSize(),
-                contentAlignment = Alignment.Center
-            ) {
-                CircularProgressIndicator()
-            }
-        }
+                    is ResultStates.Success -> {
+                        val movies =
+                            (upcomingMovies as ResultStates.Success<List<ResultMovie>>).data
+                        LazyColumn {
+                            items(movies) { movie ->
+                                MovieItem(movie = movie, onClick = { /* Handle click */ })
+                            }
+                        }
+                    }
 
-        is ResultStates.Success -> {
-            val movies = (upcomingMovies as ResultStates.Success<List<ResultMovie>>).data
-            LazyColumn {
-                items(movies) { movie ->
-                    MovieItem(movie = movie, onClick = { /* Handle click */ })
+                    is ResultStates.Error -> {
+                        Text("Error: ${(upcomingMovies as ResultStates.Error).exception.message}")
+                    }
                 }
             }
         }
-
-        is ResultStates.Error -> {
-            Text("Error: ${(upcomingMovies as ResultStates.Error).exception.message}")
-        }
-    }
-
+    )
 }
