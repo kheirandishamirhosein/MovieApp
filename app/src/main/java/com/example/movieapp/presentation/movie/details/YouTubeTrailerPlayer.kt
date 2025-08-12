7fccaa66fc0c4007fec1e2ed6290bac779d3b1b7
Amirhosein Kheirandish <kheirandishamirhosein@gmail.com>
2025-08-12 15:47:01 +0330

    Feat: Add YouTubeTrailerPlayer Composable

---
+++ app/src/main/java/com/example/movieapp/presentation/movie/details/YouTubeTrailerPlayer.kt
@@ -0,0 +1,29 @@
+package com.example.movieapp.presentation.movie.details
+
+import androidx.activity.ComponentActivity
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.AndroidView
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
+
+@Composable
+fun YouTubeTrailerPlayer(
+    videoKey: String,
+    modifier: Modifier = Modifier
+) {
+    AndroidView(
+        factory = { context ->
+            YouTubePlayerView(context).apply {
+                (context as? ComponentActivity)?.lifecycle?.addObserver(this)
+                addYouTubePlayerListener(object : AbstractYouTubePlayerListener() {
+                    override fun onReady(youTubePlayer: YouTubePlayer) {
+                        youTubePlayer.loadVideo(videoKey, 0f)
+                    }
+                })
+            }
+        },
+        modifier = modifier
+    )
+}
